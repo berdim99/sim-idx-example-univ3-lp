6a4d4942b2b91f0a1bee699ee9456d165b1ff244
Micha Berdichevsky <berdim99@gmail.com>
2025-07-18 09:27:51 -0400

    one more commit

--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -111,7 +111,6 @@ contract Listener is
         uint64 token1_decimals
     );
     /// Event to track changes in liquidity of positions
-    ///
     /// @custom:index lp_idx1 BTREE (pool, block_number);
     event LpEvents(
         bytes32 txn_hash,
